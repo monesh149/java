066da7e530964b63036d33031bfff9c3330eec9f
monesh149 <moneshravi149@gmail.com>
2025-01-23 21:02:57 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class q1
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner (System.in);
+        String name= scan.nextLine();
+        int age = scan.nextInt();
+        scan.nextLine();
+        String address = scan.nextLine();
+        System.out.println("My name is "+name);
+        System.out.println("My age is "+age);
+        System.out.print("My address is"+address);
+    }
+
+}
+ 
