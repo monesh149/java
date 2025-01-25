4bdf7b8db8c036346f4fe22cd792613677dc7995
monesh149 <moneshravi149@gmail.com>
2025-01-25 22:12:33 +0530

    Add files via upload

---
+++ q3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class demo{
+         public static void main (String args[])
+    {
+       int Score = 48;
+        
+      if(Score<50)
+      {
+        System.out.print("you need to improve");
+      }
+      else if(Score>=50 && Score<=70)
+      {
+        System.out.print("good job");
+      }
+      else if(Score>70)
+      {
+        System.out.print("Excellent");
+      }
+      }
+         }
