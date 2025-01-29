b9b2dc5d38bb56de6f8438506e333a3ee839a916
monesh149 <moneshravi149@gmail.com>
2025-01-29 22:10:00 +0530

    Add files via upload

---
+++ mon.java
@@ -0,0 +1,17 @@
+class demo
+{
+    public static void main(String args[])
+    {  int count =0;
+       for (int i=1;i<=100;i=i+1)
+       {
+
+        if(i%3==0 && i%5==0)
+       
+        { 
+            count =count+1;
+          System.out.println(i);
+        }
+    }
+}
+}
+ 
