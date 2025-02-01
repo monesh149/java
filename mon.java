041739b4277e6b14890a6c9c5c9eca961fa5f97f
monesh149 <moneshravi149@gmail.com>
2025-02-01 21:03:43 +0530

    Add files via upload

--- mon.java
+++ mon.java
@@ -1,12 +1,14 @@
-import java.util.Scanner;
-class demo
-{
+class mon {
     public static void main(String args[])
-      { 
-       for (int i=1;i<=10;i=i+1)
-       {
-        System.out.println(i+"*2="+i*2);
+    {
+        for ( int i =1; i<=3;i=i+1)
+        {
+         for ( int j =1; j<=i;j=j+1)
+         {
+            System.out.print("*");
+         }
+         System.out.println();
+        }
     }
-}
 }
  
