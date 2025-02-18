bb6fdea63dff50200c079f32031e1b3f2af9bb3b
Monesh <moneshravi149@gmail.com>
2025-02-18 21:17:48 +0530

    Add files via upload

--- mon.java
+++ mon.java
@@ -1,14 +1,59 @@
-class mon {
-    public static void main(String args[])
+import java.util.Scanner;
+class Vehicle 
+{
+    String Brand;
+    int Year;
+
+    void StartEngine() 
     {
-        for ( int i =1; i<=3;i=i+1)
-        {
-         for ( int j =1; j<=i;j=j+1)
-         {
-            System.out.print("*");
-         }
-         System.out.println();
-        }
+        System.out.println("This is Vehicle");
     }
 }
- 
+
+class Car extends Vehicle 
+{
+    String fuelType;
+
+    @Override
+    void StartEngine() 
+    {
+        System.out.println("Car engine starts");
+    }
+
+    void drive() 
+    {
+        System.out.println("Car is driving");
+    }
+}
+
+class Truck extends Vehicle 
+{
+    int LoadCapacity; 
+    @Override
+    void StartEngine() 
+    {
+        System.out.println("Truck engine starts");
+    }
+
+    void haul() 
+    {
+        System.out.println("Truck is hauling");
+    }
+}
+
+class Main { 
+    public static void main(String[] args) {
+        Truck t1 = new Truck();
+        t1.StartEngine();
+        t1.Year = 2023;
+        t1.Brand = "xxx";
+        t1.LoadCapacity = 1000; 
+        t1.haul();
+
+        Car car1 = new Car();
+        car1.Brand = "Red car";
+        car1.fuelType = "Petrol";
+        car1.StartEngine(); 
+        car1.drive();
+    }
+}
